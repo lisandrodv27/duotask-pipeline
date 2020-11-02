a4a0c947cf1c3cb6802ce3082c974dabd4ebf3df
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-02 17:26:09 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+ssh -i /home/lisandrodv27/.ssh/id_rsa lisandrodv27@34.91.152.250 << EOF 
+sudo docker pull lisandrodv27/flask-app:1.0
+sudo docker run -d -p 5000:5000 --name=flask-app lisandrodv27/flask-app:1.0
+git clone https://github.com/LukeBenson/duo-task.git
+cd duo-task
+sudo docker run -d -p 80:80 --mount type=bind,source=./duo-task/nginx.conf,target=/etc/nginx/nginx.conf --name nginx nginx:latest
+EOF
