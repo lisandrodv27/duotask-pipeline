a86c9c50d24488b5ecb7f8573a0b75860daa0db9
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-11-07 19:24:36 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh -i /home/lisandrodv27/.ssh/id_rsa lisandrodv27@34.91.152.250 << EOF 
+ssh -i /home/lisandrodv27/.ssh/id_rsa lisandrodv27@34.105.147.122 << EOF 
 sudo docker pull lisandrodv27/flask-app:1.0
 sudo docker run -d -p 5000:5000 --name=flask-app lisandrodv27/flask-app:1.0
 git clone https://github.com/LukeBenson/duo-task.git
